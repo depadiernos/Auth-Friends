7b8d707b9e3e32553db9782bdc78a51580aaabef
Dennis Padiernos <depadiernos@gmail.com>
2019-11-02 23:12:46 -0500

    implement editing and deleting friend

--- client/src/components/Friend.js
+++ client/src/components/Friend.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import AddFriend from "./AddFriend";
 
 export default function Friend(props) {
   const { friend } = props;
+  const [isEditing, setIsEditing] = useState(false);
+
+  const editFriend = () => {
+    setIsEditing(true);
+  };
+
   return (
     <div>
-      <h1>{friend.name}</h1>
-      <p>{friend.age}</p>
-      <p>{friend.email}</p>
-      <button onClick={()=>props.editFriend(friend)}>Edit</button>
-      <button onClick={()=>props.deleteFriend(friend)}>X</button>
+      {!isEditing ? (
+        <>
+          <h1>{friend.name}</h1>
+          <p>{friend.age}</p>
+          <p>{friend.email}</p>
+        </>
+      ) : (
+        <AddFriend friend={friend} addOrEditFriend={props.editFriend} setIsEditing={setIsEditing}/>
+      )}
+      <button onClick={editFriend}>Edit</button>
+      <button onClick={() => props.deleteFriend(friend)}>X</button>
     </div>
   );
 }
