a7ca1d744885e682b65f22b078f57ab2d29f6d49
Dennis Padiernos <depadiernos@gmail.com>
2019-11-02 22:51:00 -0500

    create conditional navlinks and more routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { withRouter, Route, NavLink } from "react-router-dom";
-import ProtectedRoute from './components/ProtectedRoute'
+import { getToken } from "./utils/axios";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Home from "./components/Home";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import FriendsList from "./components/FriendsList";
 
 function App() {
+  const LoggedIn = getToken();
   return (
     <div className="App">
       <nav>
-        <NavLink to="/friends">Friends List</NavLink>
-        <NavLink to="/">Login</NavLink>
+        {LoggedIn && (
+          <>
+            <NavLink to="/friends">Friends List</NavLink>
+            <NavLink to="/logout">Logout</NavLink>
+          </>
+        )}
+        {!LoggedIn && (
+          <>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </>
+        )}
       </nav>
+      <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
-      <ProtectedRoute path="/friends" component={FriendsList}/>
+      <Route exact path="/logout" component={Logout} />
+      <ProtectedRoute exact path="/friends" component={FriendsList} />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
